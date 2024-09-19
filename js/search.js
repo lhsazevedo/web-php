5864874ee9a7763f3474d8a33620338a00cde2a2
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-09-19 19:20:14 -0300

    Remove search backends

--- js/search.js
+++ js/search.js
@@ -1,61 +1,8 @@
 /**
  * A jQuery plugin to add typeahead search functionality to the navbar search
- * box.  This requires Hogan for templating and typeahead.js for the actual
- * typeahead functionality.
+ * box. This requires FuzzySearch for the actual search functionality.
  */
 (function ($) {
-    /**
-     * A backend, which encapsulates a set of completions, such as a list of
-     * functions or classes.
-     *
-     * @constructor
-     */
-    var Backend = function () {
-        this.elements = {};
-    };
-
-    /**
-     * Adds an item to the backend.
-     *
-     * @param {String} id          The item ID. It would help if this was unique.
-     * @param {String} name        The item name to use as a label.
-     * @param {String} description Explanatory text for item.
-     */
-    Backend.prototype.addItem = function (id, name, description, tag, type) {
-        this.elements[id] = {
-            id: id,
-            name: name,
-            description: description,
-            tag: tag,
-            type: type,
-        };
-    };
-
-    /**
-     * Returns the backend contents formatted as an array that typeahead.js can
-     * digest as a local data source.
-     *
-     * @return {Array}
-     */
-    Backend.prototype.toTypeaheadArray = function () {
-        var array = [];
-
-        $.each(this.elements, function (_, element) {
-            element.methodName = element.name.split('::');
-            if (element.methodName.length > 1) {
-                element.methodName = element.methodName.slice(-1)[0];
-            } else {
-                delete element.methodName;
-            }
-            array.push(element);
-        });
-
-        /**
-         * Old pre-sorting has no effect on results sorted by score.
-         */
-        return array;
-    };
-
     /**
      * The actual search plugin. Should be applied to the input that needs
      * typeahead functionality.
@@ -93,19 +40,15 @@
 
         /**
          * Processes a data structure in the format of our search-index.php
-         * files and returns an object containing multiple Backend objects.
+         * files and returns an array of search items.
          *
          * @param {Object} index
-         * @return {Object}
+         * @return {Array}
          */
         var processIndex = function (index) {
-            // The search types we want to support.
-            var backend = new Backend()
+            var items = [];
 
             $.each(index, function (id, item) {
-                /* If the item has a name, then we should figure out what type
-                 * of data this is, and hence which backend this should go
-                 * into. */
                 if (item[0]) {
                     var type = "General";
 
@@ -113,33 +56,34 @@
                         case "phpdoc:varentry":
                             type = "Variable";
                             break;
-
                         case "refentry":
                             type = "Function";
                             break;
-
                         case "phpdoc:exceptionref":
-                             type = "Exception";
-                             break;
-
+                            type = "Exception";
+                            break;
                         case "phpdoc:classref":
-                             type = "Class";
-                             break;
-
+                            type = "Class";
+                            break;
                         case "set":
                         case "book":
                         case "reference":
-                             type = "Extension";
-                             break;
+                            type = "Extension";
+                            break;
                     }
 
-                    if (type) {
-                        backend.addItem(id, item[0], item[1], item[2], type);
-                    }
+                    items.push({
+                        id: id,
+                        name: item[0],
+                        description: item[1],
+                        tag: item[2],
+                        type: type,
+                        methodName: item[0].split('::').slice(-1)[0]
+                    });
                 }
             });
 
-            return backend;
+            return items;
         };
 
         /**
@@ -147,8 +91,7 @@
          *
          * @param {String}   language The language to search for.
          * @param {Function} success  Success handler, which will be given an
-         *                            object containing multiple Backend
-         *                            objects on success.
+         *                            array of search items on success.
          * @param {Function} failure  An optional failure handler.
          */
         var loadLanguage = function (language, success, failure) {
@@ -167,9 +110,7 @@
                     // We'll use anything that's less than two weeks old.
                     since.setDate(since.getDate() - 14);
                     if (cache.time > since.getTime()) {
-                        b = new Backend();
-                        b.elements = cache.data.elements
-                        success(b);
+                        success(cache.data);
                         return;
                     }
                 }
@@ -181,16 +122,13 @@
                 error: failure,
                 success: function (data) {
                     // Transform the data into something useful.
-                    var backend = processIndex(data);
+                    var items = processIndex(data);
                     // Cache the data if we can.
                     if (canCache()) {
-                        /* This may fail in IE 8 due to exceeding the local
-                         * storage limit. If so, squash the exception: this
-                         * isn't a required part of the system. */
                         try {
                             window.localStorage.setItem(key,
                                 JSON.stringify({
-                                    data: backend,
+                                    data: items,
                                     time: new Date().getTime()
                                 })
                             );
@@ -198,20 +136,20 @@
                             // Derp.
                         }
                     }
-                    success(backend);
+                    success(items);
                 },
                 url: "/js/search-index.php?lang=" + language
             });
         };
 
         /**
-         * Actually enables the typeahead on the DOM element.
+         * Actually enables the search functionality on the DOM element.
          *
-         * @param {Object} backend An array-like object containing backend.
+         * @param {Array} items An array of search items.
          */
-        var enableSearchTypeahead = function (backend) {
+        var enableSearch = function (items) {
             var fuzzyhound = new FuzzySearch({
-                source: backend.toTypeaheadArray(),
+                source: items,
                 token_sep: ' \t.,-_', // treat colon as part of token, ignore tabs (from pasted content)
                 score_test_fused: true,
                 keys: [
@@ -275,7 +213,6 @@
                         'phpdoc:varentry',
                         'stream_wrapper',
                         'stream_context_option',
-
                     ]
 
                     if (bracesTags.includes(item.tag)) {
@@ -300,8 +237,8 @@
         };
 
         // Look for the user's language, then fall back to English.
-        loadLanguage(options.language, enableSearchTypeahead, function () {
-            loadLanguage("en", enableSearchTypeahead);
+        loadLanguage(options.language, enableSearch, function () {
+            loadLanguage("en", enableSearch);
         });
 
         return this;
